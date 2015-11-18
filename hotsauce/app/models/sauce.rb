6864a54fdf0b6a43826a75f6ea59ed4192f8791e
asilver4 <adam.silver@razorfish.com>
2015-11-17 21:31:35 -0500

    added scopes

--- hotsauce/app/models/sauce.rb
+++ hotsauce/app/models/sauce.rb
@@ -4,7 +4,15 @@ class Sauce < ActiveRecord::Base
 	has_attached_file :image#, :styles => { :medium => "300x300>", :thumb => "100x100>" }
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
-	scope :by_user, lambda { |user|
-    	where(:vegan => user.vegan)
+	scope :vegan_user, lambda { |user|
+    	where(:vegan => user.vegan) unless user.vegan == false
+  	}
+
+  	scope :gluten_user, lambda { |user|
+    	where(:no_gluten => user.no_gluten) unless user.no_gluten == false
+  	}
+
+  	scope :alcohol_user, lambda { |user|
+    	where(:no_alcohol => user.no_alcohol) unless user.no_alcohol == false
   	}
 end
